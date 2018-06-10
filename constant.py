9d1f218c48d960c6ad189ea185c55fc45260ccb4
black <179756968@qq.com>
2018-06-10 13:02:24 +0800

    change sql condition
    set turnover rate to 8%

--- constant.py
+++ constant.py
@@ -7,9 +7,9 @@ DB_NAME = "stock"
 RECOMMEND_SQL_1 = """
 select s.* from stock s
 left join daily d on s.code=d.code
-where mktcap>300000 and mktcap<1200000
+where mktcap>300000 and mktcap<1500000
 and date=(select distinct date from daily order by date desc limit 1)
-and volume/nmc > 0.05
+and volume/nmc > 0.08
 and high<settlement
 and trade<25
 and trade<open
@@ -20,9 +20,9 @@ and change_percent>-2
 RECOMMEND_SQL_2 = """
 select s.* from stock s
 left join daily d on s.code=d.code
-where mktcap>300000 and mktcap<1200000
+where mktcap>300000 and mktcap<1500000
 and date=(select distinct date from daily order by date desc limit 1)
-and volume/nmc > 0.05
+and volume/nmc > 0.08
 and high>settlement*1.06
 and trade<25
 and trade<open
